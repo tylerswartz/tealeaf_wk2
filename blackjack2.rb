a900be0514b6c49dc60779d90fa279c078a02666
Tyler Swartz <tgswartz@gmail.com>
2014-02-12 21:06:30 -0800

    blackjack game

--- blackjack2.rb
+++ blackjack2.rb
@@ -1,11 +1,13 @@
 class Card
+  attr_accessor :suit, :face_value
+
   def initialize(f,s)
     @face_value = f
     @suit = s
   end
 
   def card_output
-    puts "The card is a #{@face_value} of #{@suit}"
+    "The #{@face_value} of #{@suit}"
   end
 
   def to_s
@@ -38,18 +40,73 @@ class Deck
   def size
     cards.size
   end
+end
+
+module Hand
+  def show_hand
+    puts "---- #{name}'s Hand ----"
+    cards.each do |card|
+      puts "=> #{card}" 
+    end
+    puts "=> Total is #{total}"
+  end
+
+  def total
+    face_values = cards.map{|card| card.face_value}
+    total = 0
+  
+    face_values.each do |v|
+      if v == 'Ace'
+        total = total + 11
+      elsif v.to_i == 0
+        total = total + 10
+      else
+        total = total + v.to_i
+      end
+    end
+  
+    face_values.select{|v| v == "Ace"}.count.times do
+      total = total - 10 if total > 21
+    end
+   
+    return total  
+  end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def busted?
+    total > 21
+  end
 
 end
 
+
 class Player
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize(name)
+    @name = name
+    @cards = []
+  end
 end
 
 class Dealer
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
+  end
 end
 
-class Hand
-end
 
+#actions:
 class DealCards
 end
 
@@ -60,6 +117,20 @@ class Stay
 end
 
 deck = Deck.new
-puts deck.cards
 
-puts deck.cards.size
+player = Player.new("Tyler")
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.show_hand
+player.busted?
+
+dealer = Dealer.new
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.show_hand
+dealer.busted?
+
+
+
